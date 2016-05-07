2e8629ed231fb4ddc0b3e5bb99a5ab8509415274
gpawade <gap.subscribe@gmail.com>
2016-05-07 10:50:30 +0530

    part 1 changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,29 @@
 import {Component} from '@angular/core';
 
+class Hero {
+    id : number;          
+    name : string;
+}
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>{{title}}</h1> 
+        <h3>{{hero.name}} details</h3>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    `
+    
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Tour of heroes';
+    hero: Hero = {
+        id: 1,
+        name : 'windstrom'
+    };
+ }
